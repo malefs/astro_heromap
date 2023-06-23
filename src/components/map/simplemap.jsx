18351a587f507282938004586c493175e78ae92f
Matthias Melitzki <m.melitzki@gpi-stenn.de>
2023-06-23 18:30:49 +0200

    update multimarker map and maptest

--- src/components/map/simplemap.jsx
+++ src/components/map/simplemap.jsx
@@ -1,15 +1,55 @@
-import { MapContainer, TileLayer } from "react-leaflet";
-import tileLayer from "./tileLayer";
 
-const center = [52.22977, 21.01178];
+import { MapContainer, TileLayer, useMap, Marker,Popup } from "react-leaflet";
+//import "./leaflet.css";
+import "leaflet/dist/leaflet.css";
+
+
+
 
 const SimpleMapWrapper = () => {
-  return (
-    <MapContainer center={center} zoom={12} scrollWheelZoom={true}>
-      <TileLayer {...tileLayer} />
+	const position = [51.505, -0.09];
+  return(
+		
+		
+		
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 };
-
 export default SimpleMapWrapper;
+// const SimpleMapWrapper = () => {
+
+// 	const center = [52.22977, 21.01178];
+//   const tileLayer = {
+//     attribution:
+//       '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+//     url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+//   };
+
+// 	L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//     maxZoom: 19,
+//     attribution:
+//       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+//   }).addTo(map);
+
+
+// 	return (
+//     <MapContainer center={center} zoom={12} scrollWheelZoom={true}>
+//       <TileLayer {...tileLayer} />
+//     </MapContainer>
+//   );
+// };
+
+
+
+
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+            "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
